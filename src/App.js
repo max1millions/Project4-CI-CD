38ab90276bc34772a087003a6b7e68f41691453b
max1millions <161556613+max1millions@users.noreply.github.com>
2025-12-08 20:05:57 -0600

    add name to src

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
+          My name is Maximilian Larson and this is a submission for project 4
         </p>
         <a
           className="App-link"
